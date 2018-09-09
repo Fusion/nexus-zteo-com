1c113ddc3bee5d0967b10c42ae6ac5907e7d809d
Chris F Ravenscroft <chris@voilaweb.com>
2018-09-09 00:29:15 -0700

    Tweaked bleak theme added

---
+++ content/posts/reason-native-app-part-2.md
@@ -0,0 +1,8 @@
+---
+title: "Creating a ReasonML Native App -- Part 2"
+date: 2018-09-08T21:44:10-07:00
+tags: ["reason", "functional"]
+series: ["Reason Native App"]
+draft: true
+---
+
---
+++ theme_bleak_tweaked/static/js/cfr.js
@@ -0,0 +1,8 @@
+/*
+$(function() {
+    var clip_path = 'http://nexus.zteo.com/static/vid/hd0198.mp4';
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show(clip_path, {ambient:true});
+});
+*/
